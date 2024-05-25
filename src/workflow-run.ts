414f65604853fa83320301fa39bb64c93c28d36c
Hidetake Iwata <int128@gmail.com>
2024-05-25 20:19:14 +0900

    Replace with getWorkflowRun query (#15)

--- src/workflow-run.ts
+++ src/workflow-run.ts
@@ -1,15 +1,16 @@
 import assert from 'assert'
-import { CheckSuiteQuery } from './generated/graphql.js'
+import { GetWorkflowRunQuery } from './generated/graphql.js'
 import { CheckAnnotationLevel, CheckConclusionState } from './generated/graphql-types.js'
 
-export const getWorkflowRunSummary = (checkSuite: CheckSuiteQuery) => {
-  assert(checkSuite.node != null)
-  assert.strictEqual(checkSuite.node.__typename, 'CheckSuite')
+export const getWorkflowRunSummary = (workflowRun: GetWorkflowRunQuery) => {
+  assert(workflowRun.node != null)
+  assert(workflowRun.node.__typename === 'WorkflowRun')
+  const checkSuite = workflowRun.node.checkSuite
 
   const annotationMessages = new Set<string>()
   const annotationFailureMessages = new Set<string>()
   const conclusions = new Array<CheckConclusionState>()
-  for (const checkRun of checkSuite.node.checkRuns?.nodes ?? []) {
+  for (const checkRun of checkSuite.checkRuns?.nodes ?? []) {
     if (checkRun == null) {
       continue
     }
@@ -27,10 +28,10 @@ export const getWorkflowRunSummary = (checkSuite: CheckSuiteQuery) => {
   }
 
   let associatedPullRequest
-  assert(checkSuite.node.commit.associatedPullRequests != null)
-  assert(checkSuite.node.commit.associatedPullRequests.nodes != null)
-  if (checkSuite.node.commit.associatedPullRequests.nodes.length > 0) {
-    const pull = checkSuite.node.commit.associatedPullRequests.nodes[0]
+  assert(checkSuite.commit.associatedPullRequests != null)
+  assert(checkSuite.commit.associatedPullRequests.nodes != null)
+  if (checkSuite.commit.associatedPullRequests.nodes.length > 0) {
+    const pull = checkSuite.commit.associatedPullRequests.nodes[0]
     assert(pull != null)
     associatedPullRequest = {
       number: pull.number,

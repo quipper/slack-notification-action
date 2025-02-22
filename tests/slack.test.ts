6efbf64adaa815779d8fa2a6967905906e4e2dce
Hidetake Iwata <int128@gmail.com>
2025-02-22 14:31:28 +0900

    Fix test

--- tests/slack.test.ts
+++ tests/slack.test.ts
@@ -5,7 +5,7 @@ import { FailedJob } from '../src/workflow-run.js'
 describe('getFailedJobCause', () => {
   it('should return lostCommunicationErrorMessage when the failureAnnotationMessages contain lost communication message', () => {
     const failedJob: FailedJob = {
-      name: 'job1',
+      name: 'job',
       failureStepNames: [],
       failureAnnotationMessages: [
         'The self-hosted runner: example-vnvrd-runner-98tzt lost communication with the server. Verify the machine is running and has a healthy network connection. Anything in your workflow that terminates the runner process, starves it for CPU/Memory, or blocks its network access can cause this error.',
@@ -13,20 +13,39 @@ describe('getFailedJobCause', () => {
     }
     const templates: Templates = {
       lostCommunicationErrorMessage: 'SORRY',
+      shutdownSignalErrorMessage: 'BYE',
       mentionMessage: '@octocat',
     }
     const cause = getFailedJobCause(failedJob, templates)
     expect(cause).toStrictEqual(['SORRY'])
   })
 
+  it('should return shutdownSignalErrorMessage when the failureAnnotationMessages contain shutdown signal message', () => {
+    const failedJob: FailedJob = {
+      name: 'job',
+      failureStepNames: [],
+      failureAnnotationMessages: [
+        'The runner has received a shutdown signal. This can happen when the runner service is stopped, or a manually started runner is canceled.',
+      ],
+    }
+    const templates: Templates = {
+      lostCommunicationErrorMessage: 'SORRY',
+      shutdownSignalErrorMessage: 'BYE',
+      mentionMessage: '@octocat',
+    }
+    const cause = getFailedJobCause(failedJob, templates)
+    expect(cause).toStrictEqual(['BYE'])
+  })
+
   it('should return empty array when the messages are empty', () => {
     const failedJob: FailedJob = {
-      name: 'job2',
+      name: 'job',
       failureStepNames: [],
       failureAnnotationMessages: [],
     }
     const templates: Templates = {
       lostCommunicationErrorMessage: 'SORRY',
+      shutdownSignalErrorMessage: 'BYE',
       mentionMessage: '@octocat',
     }
     const cause = getFailedJobCause(failedJob, templates)
@@ -35,12 +54,13 @@ describe('getFailedJobCause', () => {
 
   it('should return the messages', () => {
     const failedJob: FailedJob = {
-      name: 'job3',
+      name: 'job',
       failureStepNames: ['Run make'],
       failureAnnotationMessages: ['message1', 'message2'],
     }
     const templates: Templates = {
       lostCommunicationErrorMessage: 'SORRY',
+      shutdownSignalErrorMessage: 'BYE',
       mentionMessage: '@octocat',
     }
     const cause = getFailedJobCause(failedJob, templates)

dbb15d7cf27bdf145bebd15a5311e24c7be5c528
Hidetake Iwata <int128@gmail.com>
2025-02-22 14:34:15 +0900

    Refactor

--- tests/slack.test.ts
+++ tests/slack.test.ts
@@ -3,7 +3,13 @@ import { getFailedJobCause, Templates } from '../src/slack.js'
 import { FailedJob } from '../src/workflow-run.js'
 
 describe('getFailedJobCause', () => {
-  it('should return lostCommunicationErrorMessage when the failureAnnotationMessages contain lost communication message', () => {
+  const templates: Templates = {
+    lostCommunicationErrorMessage: 'SORRY',
+    shutdownSignalErrorMessage: 'BYE',
+    mentionMessage: '@octocat',
+  }
+
+  it('returns lostCommunicationErrorMessage when the failureAnnotationMessages contain lost communication message', () => {
     const failedJob: FailedJob = {
       name: 'job',
       failureStepNames: [],
@@ -11,16 +17,11 @@ describe('getFailedJobCause', () => {
         'The self-hosted runner: example-vnvrd-runner-98tzt lost communication with the server. Verify the machine is running and has a healthy network connection. Anything in your workflow that terminates the runner process, starves it for CPU/Memory, or blocks its network access can cause this error.',
       ],
     }
-    const templates: Templates = {
-      lostCommunicationErrorMessage: 'SORRY',
-      shutdownSignalErrorMessage: 'BYE',
-      mentionMessage: '@octocat',
-    }
     const cause = getFailedJobCause(failedJob, templates)
     expect(cause).toStrictEqual(['SORRY'])
   })
 
-  it('should return shutdownSignalErrorMessage when the failureAnnotationMessages contain shutdown signal message', () => {
+  it('returns shutdownSignalErrorMessage when the failureAnnotationMessages contain shutdown signal message', () => {
     const failedJob: FailedJob = {
       name: 'job',
       failureStepNames: [],
@@ -28,40 +29,25 @@ describe('getFailedJobCause', () => {
         'The runner has received a shutdown signal. This can happen when the runner service is stopped, or a manually started runner is canceled.',
       ],
     }
-    const templates: Templates = {
-      lostCommunicationErrorMessage: 'SORRY',
-      shutdownSignalErrorMessage: 'BYE',
-      mentionMessage: '@octocat',
-    }
     const cause = getFailedJobCause(failedJob, templates)
     expect(cause).toStrictEqual(['BYE'])
   })
 
-  it('should return empty array when the messages are empty', () => {
+  it('returns empty array when the messages are empty', () => {
     const failedJob: FailedJob = {
       name: 'job',
       failureStepNames: [],
       failureAnnotationMessages: [],
     }
-    const templates: Templates = {
-      lostCommunicationErrorMessage: 'SORRY',
-      shutdownSignalErrorMessage: 'BYE',
-      mentionMessage: '@octocat',
-    }
     const cause = getFailedJobCause(failedJob, templates)
     expect(cause).toStrictEqual([])
   })
 
-  it('should return the messages', () => {
+  it('returns the messages', () => {
     const failedJob: FailedJob = {
       name: 'job',
       failureStepNames: ['Run make'],
       failureAnnotationMessages: ['message1', 'message2'],
-    }
-    const templates: Templates = {
-      lostCommunicationErrorMessage: 'SORRY',
-      shutdownSignalErrorMessage: 'BYE',
-      mentionMessage: '@octocat',
     }
     const cause = getFailedJobCause(failedJob, templates)
     expect(cause).toStrictEqual(['```', 'Run make', 'message1', 'message2', '```'])

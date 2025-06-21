8d4e491c7f25b5ae83398dea81176d3010019aff
Hidetake Iwata <int128@gmail.com>
2025-06-21 14:22:51 +0900

    Refactor

--- src/github.ts
+++ src/github.ts
@@ -1,13 +1,10 @@
 import assert from 'assert'
 import * as fs from 'fs/promises'
-import * as webhook from '@octokit/webhooks-types'
 import { Octokit } from '@octokit/action'
+import { WebhookEvent } from '@octokit/webhooks-types'
 import { retry } from '@octokit/plugin-retry'
 
-export const getOctokit = (token: string): Octokit => {
-  const OctokitWithRetry = Octokit.plugin(retry)
-  return new OctokitWithRetry({ auth: token, authStrategy: null })
-}
+export const getOctokit = (token: string) => new (Octokit.plugin(retry))({ auth: token, authStrategy: null })
 
 export type Context = {
   repo: {
@@ -15,28 +12,26 @@ export type Context = {
     repo: string
   }
   actor: string
-  eventName: string
-  payload: webhook.WebhookEvent
+  payload: WebhookEvent
   runId: number
 }
 
 export const getContext = async (): Promise<Context> => {
   // https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
-  assert(process.env.GITHUB_REPOSITORY, 'GITHUB_REPOSITORY is required')
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
-
-  assert(process.env.GITHUB_ACTOR, 'GITHUB_ACTOR is required')
-  assert(process.env.GITHUB_EVENT_NAME, 'GITHUB_EVENT_NAME is required')
-  assert(process.env.GITHUB_RUN_ID, 'GITHUB_RUN_ID is required')
-
-  assert(process.env.GITHUB_EVENT_PATH, 'GITHUB_EVENT_PATH is required')
-  const payload = JSON.parse(await fs.readFile(process.env.GITHUB_EVENT_PATH, 'utf-8')) as webhook.WebhookEvent
-
   return {
-    repo: { owner, repo },
-    actor: process.env.GITHUB_ACTOR,
-    eventName: process.env.GITHUB_EVENT_NAME,
-    payload,
-    runId: Number.parseInt(process.env.GITHUB_RUN_ID),
+    repo: getRepo(),
+    actor: getEnv('GITHUB_ACTOR'),
+    runId: Number.parseInt(getEnv('GITHUB_RUN_ID')),
+    payload: JSON.parse(await fs.readFile(getEnv('GITHUB_EVENT_PATH'), 'utf-8')) as WebhookEvent,
   }
+}
+
+const getRepo = () => {
+  const [owner, repo] = getEnv('GITHUB_REPOSITORY').split('/')
+  return { owner, repo }
+}
+
+const getEnv = (name: string): string => {
+  assert(process.env[name], `${name} is required`)
+  return process.env[name]
 }
